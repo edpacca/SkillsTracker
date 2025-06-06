86eea54e1400fd5bac1cba6629a080af0420337b
Eddie Pace <eddie@pace.me.uk>
2025-06-06 12:24:14 +0100

    add paged response to IRepo

--- SkillsTracker/Data/Repository/IRepository.cs
+++ SkillsTracker/Data/Repository/IRepository.cs
@@ -1,8 +1,11 @@
+using SkillsTracker.Models.DTOs;
+
 namespace SkillsTracker.Data.Repository;
 
 public interface IRepository<T>
     where T : class
 {
+    Task<PagedResponse<T>> GetAllPagedAsync(int page, int size, string sortBy, bool asc);
     Task<IEnumerable<T>> GetAllAsync();
     Task<T?> GetByIdAsync(int id);
     Task<T> CreateAsync(T entity);
